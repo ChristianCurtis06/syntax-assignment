362a3c2d5711e90842665edbff711f65deab5a0b
ChristianCurtis06 <ctcurtis10@gmail.com>
2024-08-20 08:14:15 -0500

    Completed Question 1

---
+++ dance-moves.py
@@ -0,0 +1,17 @@
+# Task 1: Code Correction
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+# Task 2: Your Mood Today
+user_input = input("How do you feel today? ").lower()
+
+if user_input == "happy":
+    print("That's great to hear!")
+elif user_input == "sad":
+    print("I hope your day gets better!")
+else:
+    print("Thanks for sharing!")
