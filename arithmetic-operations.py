c2befec19df9284a8191c4ff48a8e8ba8e5ac205
ChristianCurtis06 <ctcurtis10@gmail.com>
2024-08-20 09:15:22 -0500

    Completed Question 3

---
+++ arithmetic-operations.py
@@ -0,0 +1,15 @@
+# Task 1: Grocery Store Math
+cost_bacon = 5.98
+cost_lettuce = 3.18
+cost_tomatoes = 1.98
+total_cost = cost_bacon + cost_lettuce + cost_tomatoes
+
+print("The total cost of bacon, lettuce, and tomatoes is $" + str(total_cost))
+
+# Task 2: Bank Interest
+savings_amount = 15000
+interest_rate = 5 / 100
+interest_amount = savings_amount * interest_rate
+total_amount = savings_amount + interest_amount
+
+print("Your savings account with $15000 at 5 percent interest will have $" + str(total_amount) + " by the end of the year!")
